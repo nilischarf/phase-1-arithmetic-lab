1de64d55398900e457dba28c0f8a4176e10d1220
Nili Scharf <nilischarf@gmail.com>
2024-10-29 13:36:10 +0200

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10)
+    
+
+let num3 = 20
+let num4 = 8
+let mod = num3 % num4
+
+
+let max = Math.max(1, 2, 5, 6, 20)
